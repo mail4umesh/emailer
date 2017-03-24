71ceb5e6a662c731fb00657b0f35012864562162
Umesh <mail4umesh07@gmail.com>
2017-03-24 13:42:37 +0530

    Test work for Louis

--- emailer/app/Http/Controllers/NewsletterController.php
+++ emailer/app/Http/Controllers/NewsletterController.php
@@ -53,43 +53,77 @@ class NewsletterController extends Controller {
 	function add2mailChimp(){
 
 		$input = Input::All();
-		print_r($input);
+		//print_r($input);
 
 		$fname  = $input['fname'];
 		$lname  = $input['lname'];
 		$email  = $input['email'];
-		$erro = false;
-
-		try {
-			$this->mailchimp
-				->lists
-				->subscribe(
-					$this->listId,
-					['email' => $email,'merge_fields'  => [
-            		    'FNAME'     => $fname,
-            		    'LNAME'     => $lname
-            		]]
-
-				);
-        } catch (\Mailchimp_List_AlreadySubscribed $e) {
-        	// do something
-        	$erro = true;
-        	$msg = "You are already in list";
-        } catch (\Mailchimp_Error $e) {
-        	// do something
-        	$erro = true;
-        	$msg = "Error";
-        } 
-
-        return Response::json(['error'=> $error,'msg'=>$msg ], 200);
-
-
+		$result = $this->save2mailchimp($fname,$lname,$email);
+		return Response::json($result,200);
+		
 
 
 	}
 
+	function save2mailchimp($fname,$lname,$email){
 
-
+		if(!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+        	// MailChimp API credentials
+        	$apiKey = 'c837c605cc1a43b439762c09ac710061-us15';
+        	$listID = 'b6f8006b0a';
+        	
+        	// MailChimp API URL
+        	$memberID = md5(strtolower($email));
+        	$dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+        	$url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listID . '/members/' . $memberID;
+        	
+        	// member information
+        	$json = json_encode([
+        	    'email_address' => $email,
+        	    'status'        => 'subscribed',
+        	    'merge_fields'  => [
+        	        'FNAME'     => $fname,
+        	        'LNAME'     => $lname
+        	    ]
+        	]);
+        	
+        	// send a HTTP POST request with curl
+        	$ch = curl_init($url);
+        	curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
+        	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        	curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        	$result = curl_exec($ch);
+        	$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        	curl_close($ch);
+        	
+        	// store the status message based on response code
+        	if ($httpCode == 200) {
+        		$success = true;
+        	    $msg = '<strong>Success!</strong>You have successfully subscribed to Our Email World.</p>';
+        	} else {
+        	    switch ($httpCode) {
+        	        case 214:
+        	            $msg = '<strong>Fail!</strong>You are already subscribed.';
+        	            $success = false;
+        	            break;
+        	        default:
+        	            $msg = '<strong>Fail!</strong>Some problem occurred, please try again.';
+        	            $success = false;
+        	            break;
+        	    }
+        	}
+    	}else{
+    	    $msg = '<strong>Fail!</strong>Please enter valid email address.';
+    	    $success = false;
+    	}
+    	$data['success'] 	=	$success;
+    	$data['msg'] 		=	$msg;
+    	return $data;
+	}
 	
 }
 
--- emailer/resources/views/subscriber-form.blade.php
+++ emailer/resources/views/subscriber-form.blade.php
@@ -12,11 +12,12 @@
 		<h2>Subscriber Form</h2>
 		<div class="alert alert-success" style="display:none">
   			<a href="javascript:void(0)" class="alert-close pull-right" >&times;</a>
-  			<strong>Success!</strong> Thank You for subscribing.
+  			<div id="success-message"></div>
+  			
 		</div>
 		<div class="alert alert-danger" style="display:none">
   			<a href="javascript:void(0)" class="alert-close pull-right" >&times;</a>
-  			<strong>Success!</strong> Thank You for subscribing.
+  			<div id="fail-message"></div>
 		</div>
 		<input type="text" id="fname" class="form-control" name="fname" placeholder="Enter First Name">
 		<input type="text" id="lname" class="form-control" name="lname" placeholder="Enter Last Name">
@@ -44,14 +45,27 @@
 			$.ajax({
             type: "POST",
                     url : "{{ URL::to('/add2mailchimp') }}",
+                    dataType:'json',
                     data : {fname:fname, lname:lname, email:email, _token:_token },
+                    beforeSend:function(){
+                    	$(".alert-danger").hide();
+                    	$(".alert-success").hide();
+                    	$("#submit").html('<i class="fa fa-spinner"></i>Saving...');
+                    },
                     success : function(data){
                     //alert(data);
-                    if(data.error)
-                    	$(".alert-danger").show();
-                    } else {
+                    if(data.success){
+                    	$("#success-message").html(data.msg);
                     	$(".alert-success").show();
+                    	
+                    } else {
+                    	$("#fail-message").html(data.msg);
+                    	$(".alert-danger").show();
+                    	
                     }
+                    $("#submit").html('Submit');
+                }
+
             });
 
     
